33b273cdea761e7331f6256f368e1342288bdee6
Christo <christopherbwabwa@gmail.com>
2021-01-22 02:11:40 +0200

    Adding author info and question creation date in question item

--- app/Question.php
+++ app/Question.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Question extends Model
 {
@@ -16,6 +17,16 @@ class Question extends Model
     public function setTitleAttribute($value)
     {
         $this->attributes['title'] = $value;
-        $this->attributes['slug'] = str_slug($value);
+        $this->attributes['slug'] = Str::slug($value);
+    }
+    
+    public function getUrlAttribute()
+    {
+        return route('questions.show',$this->id);
+    }
+
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
     }
 }
